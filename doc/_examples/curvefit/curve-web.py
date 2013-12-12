b30ca0faee31390d443cc85e7736f889f22dc923
Paul Kienzle <pkienzle@nist.gov>
2013-12-12 15:17:37 -0500

    move example source out of the way of automatic sphinx processing

---
+++ doc/_examples/curvefit/curve-web.py
@@ -0,0 +1,84 @@
+# Fitting a curve
+# ===============
+#
+# Fitting a curve to a data set and getting uncertainties on the
+# parameters was the main reason that bumps was created, so it
+# should be very easy to do.  Let's see if it is.
+#
+# As usual, let's import the standard names:
+
+from bumps.names import *
+
+# Let's start with some data.  Here we have some x values, which
+# is the independent variable, and some y values, which represent
+# the value measured for condition x.  In this case x is 1-D, but
+# it could be a sequence of tuples instead.  We also need the
+# uncertainty on each measurement if we want to get a meaningful
+# uncertainty on the fitted parameters.
+
+x = [1,2,3,4,5,6]
+y = [2.1,4.0,6.3,8.03,9.6,11.9]
+dy = [0.05,0.05,0.2,0.05,0.2,0.2]
+
+# In this case we entered the data as a set of lists, but we could
+# just as easily have loaded it from a three-column file using
+# something like:
+#
+# .. parsed-literal::
+#
+#    data = numpy.loadtxt("data.txt").T
+#    x,y,dy = data[0,:], data[1,:], data[2,:]
+#
+# The variations are endless --- cleaning the data so that it is
+# in a fit state to model is often the hardest part in the analysis.
+
+# Next We will define the function we want to fit.  The first argument
+# to the function names the independent variable, and the remaining
+# arguments are the fittable parameters.  The parameter arguments can
+# use a bare name, or they can use name=value to indicate the default
+# value for each parameter.  Our function defines a straight like of
+# slope $m$ with intercept $b$ defaulting to 0.
+
+def line(x, m, b=0):
+    return m*x + b
+
+# We can build a curve fitting object from our function and our data.
+# This assumes that the measurement uncertainty is normally
+# distributed, with a $1-\sigma$ confidence interval *dy* for each point.
+# We can specify initial values for $m$ and $b$ when we define the
+# model. We are going to constraint the fit so $m \in [0,4]$
+# and $b \in [-5,5]$.
+
+M = Curve(line,x,y,dy,m=2,b=2)
+M.m.range(0,4)
+M.b.range(-5,5)
+
+# Every model file ends with a problem definition including a
+# list of all models and datasets which are to be fitted.
+
+problem = FitProblem(M)
+
+# The complete model file :download:`curve.py <curve.py>` looks as follows:
+#
+# .. literalinclude:: curve.py
+#
+# We can now load and run the fit:
+#
+# .. parsed-literal::
+#
+#    $ bumps.py curve.py --fit=newton --steps=100 --store=T1
+#
+# The ``--fit=newton`` option says to use the quasi-newton optimizer for
+# not more than 100 steps.  The ``--store=T1`` option says to store the
+# initial model, the fit results and any monitoring information in the
+# directory T1.
+#
+# Here is the resulting fit:
+#
+# .. plot::
+#
+#    from sitedoc import fit_model
+#    fit_model('curve.py')
+#
+# All is well: Normalized $\chi^2_N$ is close to 1 and the line goes nicely
+# through the data.
---
+++ doc/_examples/peaks/peaks.py
@@ -0,0 +1,112 @@
+from __future__ import division
+import sys
+from math import degrees, radians, sin, cos
+
+
+import numpy as np
+
+from bumps.parameter import Parameter, varying
+
+def plot(X,Y,theory,data,err):
+    import pylab
+
+    #print "theory",theory[1:6,1:6]
+    #print "data",data[1:6,1:6]
+    #print "delta",(data-theory)[1:6,1:6]
+    pylab.subplot(131)
+    pylab.pcolormesh(X,Y, data)
+    pylab.subplot(132)
+    pylab.pcolormesh(X,Y, theory)
+    pylab.subplot(133)
+    pylab.pcolormesh(X,Y, (data-theory)/(err+1))
+
+class Gaussian(object):
+    def __init__(self, A=1, xc=0, yc=0, s1=1, s2=1, theta=0, name=""):
+        self.A = Parameter(A,name=name+"A")
+        self.xc = Parameter(xc,name=name+"xc")
+        self.yc = Parameter(yc,name=name+"yc")
+        self.s1 = Parameter(s1,name=name+"s1")
+        self.s2 = Parameter(s2,name=name+"s2")
+        self.theta = Parameter(theta,name=name+"theta")
+
+    def parameters(self):
+        return dict(A=self.A,
+                    xc=self.xc, yc=self.yc,
+                    s1=self.s1, s2=self.s2,
+                    theta=self.theta)
+
+    def __call__(self, x, y):
+        height = self.A.value
+        s1 = self.s1.value
+        s2 = self.s2.value
+        t  = -radians(self.theta.value)
+        xc = self.xc.value
+        yc = self.yc.value
+        if s1==0 or s2==0: return np.zeros_like(x)
+        a =  cos(t)**2/s1**2 + sin(t)**2/s2**2
+        b = sin(2*t)*(-1/s1**2 + 1/s2**2)
+        c =  sin(t)**2/s1**2 + cos(t)**2/s2**2
+        xbar,ybar = x-xc,y-yc
+        Zf = np.exp( -0.5*(a*xbar**2 + b*xbar*ybar + c*ybar**2) )
+        #normalization=1.0/(2*np.pi*s1*s2)
+        #print "norm",np.sum(Zf)*normalization
+        total = np.sum(Zf)
+        if np.isnan(total) or total==0:
+            print "G(A,s1,s2,t,xc,yc) ->",total,(height,s1,s2,t,xc,yc)
+            print "a,b,c",a,b,c
+        return Zf/total*abs(height) if total>0 else np.zeros_like(x)
+
+class Background(object):
+    def __init__(self, C=0, name=""):
+        self.C = Parameter(C,name=name+"background")
+    def parameters(self):
+        return dict(C=self.C)
+    def __call__(self, x, y):
+        return self.C.value
+
+class Peaks(object):
+    def __init__(self, parts, X, Y, data, err):
+        self.X,self.Y,self.data,self.err = X, Y, data, err
+        self.parts = parts
+
+    def numpoints(self):
+        return np.prod(self.data.shape)
+
+    def parameters(self):
+        return [p.parameters() for p in self.parts]
+
+    def theory(self):
+        #return self.parts[0](self.X,self.Y)
+        #parts = [M(self.X,self.Y) for M in self.parts]
+        #for i,p in enumerate(parts):
+        #    if np.any(np.isnan(p)): print "NaN in part",i
+        return sum(M(self.X,self.Y) for M in self.parts)
+
+    def residuals(self):
+        #if np.any(self.err ==0): print "zeros in err"
+        return (self.theory()-self.data)/(self.err+1)
+
+    def nllf(self):
+        R = self.residuals()
+        #if np.any(np.isnan(R)): print "NaN in residuals"
+        return 0.5*np.sum(R**2)
+
+    def __call__(self):
+        return 2*self.nllf()/self.dof
+
+    def plot(self, view='linear'):
+        plot(self.X, self.Y, self.theory(), self.data, self.err)
+
+    def save(self, basename):
+        import json
+        pars = [(p.name,p.value) for p in varying(self.parameters())]
+        out = json.dumps(dict(theory=self.theory().tolist(),
+                              data=self.data.tolist(),
+                              err=self.err.tolist(),
+                              X = self.X.tolist(),
+                              Y = self.Y.tolist(),
+                              pars = pars))
+        open(basename+".json","w").write(out)
+
+    def update(self):
+        pass
