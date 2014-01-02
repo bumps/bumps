079d1225d357cec0225f8b6add10e08ba3058a10
Paul Kienzle <pkienzle@nist.gov>
2014-01-02 10:04:24 -0500

    cleaner 2/3 interface

--- bumps/lib/bumpsmodule.cc
+++ bumps/lib/bumpsmodule.cc
@@ -147,45 +147,43 @@ static PyMethodDef methods[] = {
 	{0}
 } ;
 
-#define MODULE_NAME "bumpsmodule"
+
 #define MODULE_DOC "Bumps C Library"
-#if PY_MAJOR_VERSION >= 3
-static struct PyModuleDef moduledef = {
-    PyModuleDef_HEAD_INIT,
-    MODULE_NAME,         /* m_name */
-    MODULE_DOC,          /* m_doc */
-    -1,                  /* m_size */
-    methods,             /* m_methods */
-    NULL,                /* m_reload */
-    NULL,                /* m_traverse */
-    NULL,                /* m_clear */
-    NULL,                /* m_free */
-};
-#endif
+#define MODULE_NAME "bumpsmodule"
+#define MODULE_INIT2 initbumpsmodule
+#define MODULE_INIT3 PyInit_bumpsmodule
+#define MODULE_METHODS methods
 
+/* ==== boilerplate python 2/3 interface bootstrap ==== */
 
 #if PY_MAJOR_VERSION >= 3
-    #define MODULE_INIT(name) extern "C" PyMODINIT_FUNC PyInit_##name(void)
-#else
-    #define MODULE_INIT(name) extern "C" PyMODINIT_FUNC init##name(void)
-#endif
 
-
-
-#if defined(WIN32) && !defined(__MINGW32__)
-__declspec(dllexport)
-#endif
-
-MODULE_INIT(bumpsmodule) {
-#if PY_MAJOR_VERSION >= 3
+  PyMODINIT_FUNC MODULE_INIT3(void)
+  {
+    static struct PyModuleDef moduledef = {
+      PyModuleDef_HEAD_INIT,
+      MODULE_NAME,         /* m_name */
+      MODULE_DOC,          /* m_doc */
+      -1,                  /* m_size */
+      MODULE_METHODS,      /* m_methods */
+      NULL,                /* m_reload */
+      NULL,                /* m_traverse */
+      NULL,                /* m_clear */
+      NULL,                /* m_free */
+    };
     return PyModule_Create(&moduledef);
+  }
 
-#else
-    Py_InitModule4("bumpsmodule",
-		 methods,
-		 "Bumps C Library",
+#else /* PY_MAJOR_VERSION >= 3 */
+
+  PyMODINIT_FUNC MODULE_INIT2(void)
+  {
+    Py_InitModule4(MODULE_NAME,
+		 MODULE_METHODS,
+		 MODULE_DOC,
 		 0,
 		 PYTHON_API_VERSION
 		 );
-#endif
-}
+  }
+
+#endif /* PY_MAJOR_VERSION >= 3 */
