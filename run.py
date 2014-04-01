3b5bd3883d8faf2ae12892e25b878facb91fa496
Paul Kienzle <pkienzle@gmail.com>
2014-04-01 12:19:25 +0100

    allow run.py to be used with multiprocessing in windows

--- run.py
+++ run.py
@@ -9,18 +9,6 @@ Usage:
 
 import os, sys
 
-from distutils.util import get_platform
-platform = '.%s-%s'%(get_platform(),sys.version[:3])
-
-# Make sure that we have a private version of mplconfig
-mplconfig = os.path.join(os.getcwd(), '.mplconfig')
-os.environ['MPLCONFIGDIR'] = mplconfig
-if not os.path.exists(mplconfig): os.mkdir(mplconfig)
-#import matplotlib
-#matplotlib.use('Agg')
-#print matplotlib.__file__
-#import pylab; pylab.hold(False)
-
 def addpath(path):
     """
     Add a directory to the python path environment, and to the PYTHONPATH
@@ -42,25 +30,40 @@ def cd(path):
     yield
     os.chdir(old_dir)
 
-sys.dont_write_bytecode = True
+def prepare():
+    # Make sure that we have a private version of mplconfig
+    mplconfig = os.path.join(os.getcwd(), '.mplconfig')
+    os.environ['MPLCONFIGDIR'] = mplconfig
+    if not os.path.exists(mplconfig): os.mkdir(mplconfig)
+    #import matplotlib
+    #matplotlib.use('Agg')
+    #print matplotlib.__file__
+    #import pylab; pylab.hold(False)
 
-#import numpy; numpy.seterr(all='raise')
-root = os.path.abspath(os.path.dirname(__file__))
+    from distutils.util import get_platform
+    platform = '.%s-%s'%(get_platform(),sys.version[:3])
 
-# Force a rebuild
-import subprocess
-devnull = open('/dev/null', 'w')
-with cd(root):
-    subprocess.call((sys.executable, "setup.py", "build"), shell=False, stdout=devnull)
+    sys.dont_write_bytecode = True
 
-# Add the build dir to the system path
-build_path = os.path.join(root, 'build','lib'+platform)
-addpath(build_path)
+    #import numpy; numpy.seterr(all='raise')
+    root = os.path.abspath(os.path.dirname(__file__))
 
-# Make sample data and models available
-os.environ['BUMPS_DATA'] = os.path.join(root,'bumps','gui','resources')
+    # Force a rebuild
+    import subprocess
+    devnull = open('/dev/null', 'w') if not sys.platform.startswith('win') else None
+    with cd(root):
+        subprocess.call((sys.executable, "setup.py", "build"), shell=False, stdout=devnull)
 
-import multiprocessing
-multiprocessing.freeze_support()
-import bumps.cli
-bumps.cli.main()
+    # Add the build dir to the system path
+    build_path = os.path.join(root, 'build','lib'+platform)
+    addpath(build_path)
+
+    # Make sample data and models available
+    os.environ['BUMPS_DATA'] = os.path.join(root,'bumps','gui','resources')
+
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
+    prepare()
+    import bumps.cli
+    bumps.cli.main()
