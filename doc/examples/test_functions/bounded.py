2293ecf40abdb3e60aee9ddc988ee0b879691602
Paul Kienzle <pkienzle@nist.gov>
2015-05-07 11:07:38 -0400

    play with bounded uniform examples

--- doc/examples/test_functions/bounded.py
+++ doc/examples/test_functions/bounded.py
@@ -13,26 +13,24 @@ from bumps.names import *
 # Adjust scale from 1e-150 to 1e+150 and you will see that DREAM is equally
 # adept at filling the box.
 
-scale = 10
+scale = 1
 
 # Uniform cost function.
 
-def uniform(x):
+def box(x):
     return 0 if np.all(np.abs(x)<=scale) else np.inf
 
+def diamond(x):
+    return 0 if np.sum(np.abs(x))<=scale else np.inf
+
 # Wrap it in a PDF object which turns an arbitrary probability density into
-# a fitting function.  Give it an initial value away from the cross.
+# a fitting function.  Give it a valid initial value, and set the bounds to
+# a unit cube with one corner at the origin.
 
-M = SimplePDF(uniform)
-
-# Set the range of values to include the cross.  You can skip the center of
-# the cross by setting b.range to (1,3), and for reasonable values of sigma
-# both arms will still be covered.  Extend the range too far (e.g.,
-# a.range(-3000,3000), b.range(-1000,3000)), and like a value of sigma
-# that is too small, only one arm of the cross will be filled.
-
-M.a.range(-3*scale,3*scale)
-M.b.range(-1*scale,3*scale)
+M = PDF(lambda a,b: box([a,b]))
+#M = PDF(lambda a,b: diamond([a,b]))
+M.a.range(-2*scale,2*scale)
+M.b.range(-2*scale,2*scale)
 
 # Make the PDF a fit problem that bumps can process.
 
