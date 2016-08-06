ef7dfdfefec46c0ae1debd9296cd899cbb4f9840
Paul Kienzle <pkienzle@nist.gov>
2016-08-05 20:08:43 -0400

    R 0.7.6

--- bumps/__init__.py
+++ bumps/__init__.py
@@ -12,7 +12,7 @@ A graphical interface allows direct manipulation of the model parameters.
 See http://www.reflectometry.org/danse/reflectometry for online manuals.
 """
 
-__version__ = "0.7.5.10"
+__version__ = "0.7.6"
 
 
 def data_files():
