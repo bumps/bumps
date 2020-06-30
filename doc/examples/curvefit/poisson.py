a8315cbc8942fdd0bb6c82e2ed832cd929290c5b
Paul Kienzle <pkienzle@nist.gov>
2020-06-30 10:59:47 -0400

    lint

--- doc/examples/curvefit/poisson.py
+++ doc/examples/curvefit/poisson.py
@@ -118,7 +118,7 @@ def peak(x, scale, center, width, background):
 # data, and the returned parameters are consistent from run to run.  Real
 # data is likely not so heavily sampled.
 
-x = np.linspace(5,20,345)
+x = np.linspace(5, 20, 345)
 #y = np.random.poisson(peak(x, 1000, 12, 1.0, 1))
 #y = np.random.poisson(peak(x, 300, 12, 1.5, 1))
 y = np.random.poisson(peak(x, 3, 12, 1.5, 1))
@@ -133,38 +133,38 @@ y = np.random.poisson(peak(x, 3, 12, 1.5, 1))
 # in the fit.
 
 cond = sys.argv[1] if len(sys.argv) > 1 else "pearson"
-if cond=="poisson": # option 0: use PoissonCurve rather than Curve to fit
+if cond == "poisson": # option 0: use PoissonCurve rather than Curve to fit
     pass
-elif cond=="expected": # option 1: L = (y+1) +/- sqrt(y+1)
+elif cond == "expected": # option 1: L = (y+1) +/- sqrt(y+1)
     y += 1
     dy = np.sqrt(y)
-elif cond=="pearson": # option 2: L = (y + 0.5)  +/- sqrt(y + 1/4)
+elif cond == "pearson": # option 2: L = (y + 0.5)  +/- sqrt(y + 1/4)
     dy = np.sqrt(y+0.25)
     y = y + 0.5
-elif cond=="expected_mle": # option 3: L = y +/- sqrt(y+1)
+elif cond == "expected_mle": # option 3: L = y +/- sqrt(y+1)
     dy = np.sqrt(y+1)
-elif cond=="pearson_zero": # option 4: L = y +/- sqrt(y); L[0] = 0.5 +/- 0.5
+elif cond == "pearson_zero": # option 4: L = y +/- sqrt(y); L[0] = 0.5 +/- 0.5
     dy = np.sqrt(y)
     y = np.asarray(y, 'd')
-    y[y==0] = 0.5
-    dy[y==0] = 0.5
+    y[y == 0] = 0.5
+    dy[y == 0] = 0.5
 elif cond=="expected_zero": # option 5: L = y +/- sqrt(y);  L[0] = 0 +/- 1
     dy = np.sqrt(y)
-    dy[y==0] = 1.0
+    dy[y == 0] = 1.0
 else:
     raise RuntimeError("Need to select uncertainty: pearson, pearson_zero, expected, expected_zero, expected_mle, poisson")
 
 # Build the fitter, and set the range on the fit parameters.
 
 if cond == "poisson":
-    M = PoissonCurve(peak,x,y,scale=1,center=2,width=2,background=0)
+    M = PoissonCurve(peak, x, y, scale=1, center=2, width=2, background=0)
 else:
-    M = Curve(peak,x,y,dy,scale=1,center=2,width=2,background=0)
+    M = Curve(peak, x, y, dy, scale=1, center=2, width=2, background=0)
 dx = max(x)-min(x)
-M.scale.range(0,max(y)*1.5)
-M.center.range(min(x)-0.2*dx,max(x)+0.2*dx)
-M.width.range(0,0.7*dx)
-M.background.range(0,max(y))
+M.scale.range(0, max(y)*1.5)
+M.center.range(min(x)-0.2*dx, max(x)+0.2*dx)
+M.width.range(0, 0.7*dx)
+M.background.range(0, max(y))
 
 # Set the fit problem as usual.
 
