05091abb2d60bc3927591b039b206c072efa23e1
Brian Ben Maranville <brian.maranville@nist.gov>
2022-12-09 12:58:48 -0500

    write unhashed filenames for production mode

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({mode}) => {
+  return defineConfig({
+    plugins: [vue()],
+    base: '',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      // By default, Vite doesn't include shims for NodeJS/
+      // necessary for segment analytics lib to work
+      global: {},
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          // Default
+          // dir: 'dist',
+          entryFileNames: (mode == 'production') ? 'assets/[name].js' : 'assets/[name].[hash].js',
+          assetFileNames: (mode == 'production') ? 'assets/[name][extname]' : undefined,
+          // chunkFileNames: "chunk-[name].js",
+          // manualChunks: undefined,
+        }
+      }
     }
-  },
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-  }
-})
+  })
+}
