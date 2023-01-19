23d462ef302c599743a93f1137a510641e3d8ab6
Brian Ben Maranville <brian.maranville@nist.gov>
2023-01-18 19:12:52 -0500

    use Vue to show/hide panels instead of Tab function from Bootstrap

--- client/src/components/PanelTabContainer.vue
+++ client/src/components/PanelTabContainer.vue
@@ -2,13 +2,12 @@
 import { ref, onMounted, watch, onUpdated, computed, shallowRef } from 'vue';
 import type { DefineComponent, Component } from 'vue';
 import type { Socket } from 'socket.io-client';
-import { Tab } from 'bootstrap/dist/js/bootstrap.esm';
 
 // type PanelComponent = DefineComponent<{socket: Socket, visible: boolean}, any>;
 
 const tabTriggers = ref<HTMLAnchorElement[]>([]);
 const panelContainers = ref<HTMLDivElement[]>([]);
-const panelVisible = ref<boolean[]>([]);
+const activePanel = ref(0);
 
 const props = defineProps<{
   socket: Socket,
@@ -16,44 +15,19 @@ const props = defineProps<{
   initially_active?: number
 }>();
 
-let triggers: Tab[] = [];
-
-function activateTab(index) {
-  triggers?.[index].show();
-  panelContainers.value.forEach((el, i) => {
-    if (i === index) {
-      el.classList.add('active');
-      panelVisible.value[i] = true;
-    }
-    else {
-      el.classList.remove('active');
-      panelVisible.value[i] = false;
-    }
-  });
-}
-
 onMounted(() => {
-  for (let triggerEl of tabTriggers.value) {
-    let tabTrigger = new Tab(triggerEl);
-    triggers.push(tabTrigger);
-  }
-  if (triggers.length > 0) {
-    activateTab(props.initially_active ?? 0);
-  }
+  activePanel.value = props.initially_active ?? 0;
 });
 </script>
 
 <template>
   <ul class="nav nav-tabs">
     <li class="nav-item" v-for="(panel, index) in props.panels" :key="index">
-      <a ref="tabTriggers" class="nav-link" href="#" @click="activateTab(index)">{{panels[index]?.title}}</a>
+      <a ref="tabTriggers" :class="{'nav-link': true, active: index == activePanel}" href="#" @click="activePanel = index">{{panels[index]?.title}}</a>
     </li>
   </ul>
   <div class="tab-content d-flex flex-column flex-grow-1 overflow-auto">
-    <div ref="panelContainers" class="tab-pane flex-column flex-grow-1" v-for="(panel, index) in props.panels"
-      :key="index">
-      <component :is="panel.component" :socket="props.socket" :visible="panelVisible[index]"></component>
-    </div>
+      <component :is="panels[activePanel].component" :socket="props.socket" :visible="true"></component>
   </div>
 </template>
 
