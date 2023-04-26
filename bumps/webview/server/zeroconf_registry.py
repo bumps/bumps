3ddd66cee12ecc8f4f39629422d2e0e9ddd6c85a
bbm <brian.maranville@nist.gov>
2023-04-26 15:12:04 -0400

    clean up registry

--- bumps/webview/server/zeroconf_registry.py
+++ bumps/webview/server/zeroconf_registry.py
@@ -5,6 +5,9 @@ from zeroconf import IPVersion
 from zeroconf.asyncio import AsyncServiceInfo, AsyncZeroconf
 import datetime
 
+DEBUG = False
+
+
 class LocalRegistry:
     def __init__(self, ip_version: IPVersion = IPVersion.All, type_ : str = "_bumps._tcp.local."):
         self.interfaces = ["127.0.0.1"]
@@ -45,52 +48,6 @@ class LocalRegistry:
     def close(self):
         return self.aiozc.async_unregister_all_services()
 
-def register_local_instance(port: int, name: str, properties: Optional[Dict[str, str]]=None, _type: str="_bumps._tcp.local."):
-    aiozc = AsyncZeroconf(ip_version=IPVersion.All, interfaces=["127.0.0.1"])
-    properties = properties if properties is not None else {}
-    properties.update({"start_time": datetime.datetime.now().timestamp()})
-    info = AsyncServiceInfo(
-        _type,
-        f"{name}.{_type}",
-        addresses=["127.0.0.1"],
-        port=port,
-        properties={},
-        host_ttl = 5,
-        other_ttl = 10,
-        # properties=properties
-    )
-    return aiozc.async_register_service(info)
-
-def unregister_local_instance(port: int, name: str, properties: Optional[Dict[str, str]]=None, _type: str="_bumps._tcp.local."):
-    aiozc = AsyncZeroconf(ip_version=IPVersion.All, interfaces=["127.0.0.1"])
-    info = AsyncServiceInfo(
-        _type,
-        f"{name}.{_type}",
-        addresses=["127.0.0.1"],
-        port=port,
-        properties={},
-        host_ttl = 5,
-        other_ttl = 10
-    )
-    return aiozc.async_unregister_service(info)
-
-def unregister_all_services():
-    aiozc = AsyncZeroconf(ip_version=IPVersion.All, interfaces=["127.0.0.1"])
-    return aiozc.async_unregister_all_services()
-
-
-
-async def unregister_instance(host, port, name, path=""):
-    aiozc = AsyncZeroconf(ip_version=IPVersion.All)
-    info = AsyncServiceInfo(
-        "_http._tcp.local.",
-        name,
-        addresses=[host],
-        port=port,
-        properties={"path": path}
-    )
-    return await aiozc.async_unregister_service(info)
-
 
 # adapted from https://github.com/python-zeroconf/python-zeroconf/blob/master/examples/async_registration.py
 from zeroconf import IPVersion, ServiceStateChange, Zeroconf
@@ -100,6 +57,7 @@ from zeroconf.asyncio import (
     AsyncZeroconf,
     AsyncZeroconfServiceTypes,
 )
+
 
 class AsyncRunner:
     def __init__(self, sio, service="_bumps._tcp.local.") -> None:
@@ -112,7 +70,8 @@ class AsyncRunner:
     def on_service_state_change(self, 
         zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange
     ) -> None:
-        print(f"Service {name} of type {service_type} state changed: {state_change}")
+        if DEBUG:
+            print(f"Service {name} of type {service_type} state changed: {state_change}")
         asyncio.ensure_future(self._handle_state_change(zeroconf, service_type, name, state_change))
 
     async def _handle_state_change(self, zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange) -> None:
@@ -120,7 +79,8 @@ class AsyncRunner:
         await info.async_request(zeroconf, 3000)
         stripped_name = name.replace(f".{service_type}", '')
         
-        # print("Info from zeroconf.get_service_info: %r" % (info))
+        if DEBUG:
+            print("Info from zeroconf.get_service_info: %r\n" % (info))
         serializable_properties = dict((k.decode(), v.decode()) for k,v in info.properties.items())
         if info:
             if state_change is ServiceStateChange.Added:
@@ -132,8 +92,9 @@ class AsyncRunner:
 
             await self.sio.emit("update")
         else:
-            print("  No info")
-        print('\n')
+            # no info
+            if DEBUG:
+                print("  No info\n")
 
     def get_servers(self, sid=""):
         return self.servers
@@ -141,13 +102,10 @@ class AsyncRunner:
     async def async_run(self) -> None:
         self.aiozc = AsyncZeroconf(ip_version=IPVersion.All, interfaces=["127.0.0.1"])
 
-        # services = ["_http._tcp.local.", "_hap._tcp.local."]
         services = [self.service]
-        print(f"services: {services}")
         self.aiobrowser = AsyncServiceBrowser(
             self.aiozc.zeroconf, services, handlers=[self.on_service_state_change]
         )
-        print(self.aiobrowser)
 
     async def async_close(self) -> None:
         assert self.aiozc is not None
