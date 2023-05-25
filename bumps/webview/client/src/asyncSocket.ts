a2334409c53b8ac36dd3c9390a9415072a0b4a81
bbm <brian.maranville@nist.gov>
2023-05-25 10:55:00 -0400

    modify asyncEmit to handle callbacks

--- bumps/webview/client/src/asyncSocket.ts
+++ bumps/webview/client/src/asyncSocket.ts
@@ -7,15 +7,31 @@ declare module 'socket.io-client' {
 }
 
 Socket.prototype.asyncEmit = async function asyncEmit(ev: string, ...args: any[]) {
+  const callback = (args[args.length-1] instanceof Function) ? args.pop() : null;
   return new Promise((resolve, reject) => {
+    // get result of plain emit:
     this.emit(ev, ...args, resolve);
+  }).then( async (result) =>  {
+    // execute (possibly async) callback and then return promise of result
+    if (callback !== null) {
+      await callback(result);
+    }
+    return result;
   })
 };
 
 export class AsyncSocket extends Socket {
   async asyncEmit(ev: string, ...args: any[]) {
+    const callback = (args[args.length-1] instanceof Function) ? args.pop() : null;
     return new Promise((resolve, reject) => {
+      // get result of plain emit:
       this.emit(ev, ...args, resolve);
-    });
+    }).then( async (result) =>  {
+      // execute (possibly async) callback and then return promise of result
+      if (callback !== null) {
+        await callback(result);
+      }
+      return result;
+    })
   }
 }
