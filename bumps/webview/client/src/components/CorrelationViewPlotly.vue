3e6db6ff47bb2400730ffa4c75199d040ce1a567
bbm <brian.maranville@nist.gov>
2023-07-11 11:37:24 -0400

    use server cache for correlation plot, and trigger single plot view on click of subplot

--- bumps/webview/client/src/components/CorrelationViewPlotly.vue
+++ bumps/webview/client/src/components/CorrelationViewPlotly.vue
@@ -4,7 +4,6 @@ import { ref } from 'vue';
 import type { AsyncSocket } from '../asyncSocket';
 import { v4 as uuidv4 } from 'uuid';
 import { setupDrawLoop } from '../setupDrawLoop';
-import { cache } from '../plotcache';
 import * as Plotly from 'plotly.js/lib/core';
 import Heatmap from 'plotly.js/lib/heatmap';
 
@@ -17,11 +16,14 @@ Plotly.register([
 ])
 
 const title = "Correlations";
-const plot_div = ref<HTMLDivElement>();
+const plot_div = ref<Plotly.PlotlyHTMLElement>();
 const plot_div_id = ref(`div-${uuidv4()}`);
 const do_sort = ref(true);
 const max_rows = ref(25);
 const n_bins = ref(50);
+const callback_registered = ref(false);
+const stored_timestamp = ref("");
+const vars = ref<number[]>([]);
 
 const props = defineProps<{
   socket: AsyncSocket,
@@ -30,43 +32,63 @@ const props = defineProps<{
 setupDrawLoop('uncertainty_update', props.socket, fetch_and_draw, title);
 
 
-async function fetch_and_draw(latest_timestamp: string) {
-  let { timestamp, plotdata } = cache[title] as { timestamp: string, plotdata: Plotly.PlotlyDataLayoutConfig} ?? {};
-  if (timestamp !== latest_timestamp) {
-    const payload = await props.socket.asyncEmit('get_correlation_plot', do_sort.value, max_rows.value, n_bins.value) as Plotly.PlotlyDataLayoutConfig;
-    plotdata = { ...payload };
-    cache[title] = {timestamp: latest_timestamp, plotdata};
+async function fetch_and_draw(latest_timestamp?: string) {
+  // use stored value of timestamp if none is provided, otherwise update stored value:
+  if (latest_timestamp !== undefined) {
+    stored_timestamp.value = latest_timestamp;
   }
+  // send timestamp to control server-side cache
+  const timestamp = stored_timestamp.value;
+  const output_vars = (vars.value.length > 0) ? [...vars.value] : null;
+  const payload = await props.socket.asyncEmit('get_correlation_plot', do_sort.value, max_rows.value, n_bins.value, output_vars, timestamp) as Plotly.PlotlyDataLayoutConfig;
+  const plotdata = { ...payload };
   const { data, layout } = plotdata;
   const config = { responsive: true, scrollZoom: true }
   await Plotly.react(plot_div_id.value, [...data], layout, config);
+
+  if (!callback_registered.value) {
+    if (plot_div.value?.on) {
+      plot_div.value.on('plotly_click', (ev) => {
+        // we are putting an array of numbers in customdata on the server side.
+        vars.value = (ev as Plotly.PlotMouseEvent).points[0].data.customdata as number[];
+        fetch_and_draw();
+      });
+      callback_registered.value = true;
+    }
+  }
+}
+
+function reset_vars() {
+  vars.value = [];
+  fetch_and_draw();
 }
 
 </script>
 
 <template>
   <div class="container d-flex flex-grow-1 flex-column">
-    <div class="row g-3">
-      <div class="col-md-4 align-middle text-end">
-        <label class="form-check-label pe-2" for="sort-by-correlation">Sort by correlation </label>
-        <input class="form-check-input" type="checkbox" v-model="do_sort" id="sort-by-correlation" @change="fetch_and_draw" />
+    <div class="row g-1">
+      <div class="col-3 form-check align-items-center align-middle text-start">
+        <input class="form-check-input" type="checkbox" v-model="do_sort" id="sort-by-correlation" @change="fetch_and_draw()" />
+        <label class="form-check-label pe-2 w-auto text-wrap" for="sort-by-correlation">Sort by correlation </label>
       </div>
-      <div class="col-md-4 align-middle">
-        <label class="form-label" for="max-rows">Max. rows</label>
-        <input class="form-control" type="number" v-model="max_rows" id="max-rows" @change="fetch_and_draw" />
+      <div class="col-5 d-flex flex-row align-items-center">
+        <label class="col-form-label w-auto text-nowrap flex-fill" for="max-rows">Max. rows</label>
+        <input class="form-control flex-fill" type="number" v-model="max_rows" id="max-rows" @change="fetch_and_draw()" />
       </div>
-      <div class="col-md-4 align-middle">
-        <label class="form-label" for="n-bins">Num. bins</label>
-        <input class="form-control" type="number" v-model="n_bins" id="n-bins" @change="fetch_and_draw" />
+      <div class="col-4 d-flex flex-row align-items-center">
+        <label class="col-form-label w-auto" for="n-bins">Bins</label>
+        <input class="form-control flex-fill" type="number" v-model="n_bins" id="n-bins" @change="fetch_and_draw()" />
       </div>
     </div>
-    <div class="flex-grow-1" ref="plot_div" :id="plot_div_id">
+    <button class="btn btn-primary" v-if="vars.length > 0" @click="reset_vars">Reset plot</button>
+    <div class="flex-grow-1 plot-div" ref="plot_div" :id="plot_div_id">
     </div>
   </div>
 </template>
 
 <style scoped>
-svg {
-  width: 100%;
+div.plot-div >>> g.hovertext > path {
+  opacity: 0.5;
 }
 </style>
