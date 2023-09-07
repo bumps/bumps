26a2b44afc7ed49a5c78e3205cbce76260d82cc7
bbm <brian.maranville@nist.gov>
2023-09-07 10:47:26 -0400

    update base state.py to match current interface used in api

--- bumps/webview/server/state.py
+++ bumps/webview/server/state.py
@@ -1,16 +1,23 @@
 from collections import deque
 from queue import Queue
 from typing import Dict, List, Optional, TYPE_CHECKING
+from threading import Event
 import asyncio
+
+from .state_hdf5_backed import SERIALIZERS
 
 if TYPE_CHECKING:
     import bumps, bumps.fitproblem, bumps.dream.state
     from .webserver import TopicNameType
     from .fit_thread import FitThread
 
+
+SESSION_FILE_NAME = "session.h5"
+
 class ProblemState:
     fitProblem: Optional['bumps.fitproblem.FitProblem'] = None
     pathlist: Optional[List[str]] = None
+    serializer: Optional[SERIALIZERS] = None
     filename: Optional[str] = None
 
 class FittingState:
@@ -23,10 +30,15 @@ class State:
     # These attributes are ephemeral, not to be serialized/stored:
     hostname: str
     port: int
+    parallel: int
     abort_queue: Queue
-    fit_stopped_future: Optional[asyncio.Future] = None
     fit_thread: Optional['FitThread'] = None
+    fit_abort: Optional[Event] = None
+    fit_abort_event: Event
+    fit_complete_event: Event
     calling_loop: Optional[asyncio.AbstractEventLoop] = None
+    fit_enabled: Event
+    session_file_name: Optional[str]
 
     # State to be stored:
     problem: ProblemState
@@ -36,7 +48,8 @@ class State:
     def __init__(self, problem: Optional[ProblemState] = None, fitting: Optional[FittingState] = None):
         self.problem = problem if problem is not None else ProblemState()
         self.fitting = FittingState()
-        self.abort_queue = Queue()
+        self.fit_abort_event = Event()
+        self.fit_complete_event = Event()
         self.topics = {
             "log": deque([]),
             "update_parameters": deque([], maxlen=1),
@@ -49,5 +62,25 @@ class State:
             "fitter_active": deque([], maxlen=1),
         }
 
+    def setup_backing(self, session_file_name: Optional[str] = SESSION_FILE_NAME, read_only: bool = False ):
+        self.session_file_name = session_file_name
+
+    def copy_session_file(self, session_copy_name: str):
+        pass
+
+    def write_session_file(self):
+        pass
+
+    def cleanup(self):
+        self._session_file.close()
+
+    def __del__(self):
+        self.cleanup()
+
+    async def async_cleanup(self):
+        self.cleanup()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.cleanup()
     async def cleanup(self):
         pass
