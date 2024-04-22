a460ac76f9477a188b3fa2a9d4c13b7f36055126
bbm <brian.maranville@nist.gov>
2024-04-22 15:18:24 -0400

    add toggle switch for turning labels and x-axis tick labels off in uncertainty plots (useful when n_plots > 12)

--- bumps/webview/client/src/components/UncertaintyView.vue
+++ bumps/webview/client/src/components/UncertaintyView.vue
@@ -23,6 +23,15 @@ const plot_div_id = ref(`div-${uuidv4()}`);
 const props = defineProps<{
   socket: AsyncSocket,
 }>();
+const show_labels = ref(true);
+
+async function on_change_show_labels() {
+  let { timestamp, plotdata } = cache[title] as { timestamp: string, plotdata: Plotly.PlotlyDataLayoutConfig } ?? {};
+  if (plotdata) {
+    apply_label_visibility(plotdata.layout, show_labels.value);
+    await Plotly.react(plot_div_id.value, [...plotdata.data], plotdata.layout);
+  }
+}
 
 const { drawing_busy } = setupDrawLoop('uncertainty_update', props.socket, fetch_and_draw, title);
 
@@ -38,13 +47,34 @@ async function fetch_and_draw(latest_timestamp: string) {
   delete layout?.width;
   delete layout?.height;
   const config = { responsive: true, scrollZoom: true, modeBarButtonsToAdd: [ SVGDownloadButton ] };
+  apply_label_visibility(layout, show_labels.value);
   await Plotly.react(plot_div_id.value, [...data], layout, config);
+}
+
+function apply_label_visibility(layout: Plotly.Layout, visible: boolean) {
+  layout.showlegend = visible;
+  for (let item_name in layout) {
+    if (item_name.startsWith('xaxis')) {
+      layout[item_name].showticklabels = visible;
+    }
+  }
+  for (let annotation of layout?.annotations ?? []) {
+    if (annotation?.name === 'label') {
+      annotation.visible = visible;
+    }
+  }
 }
 
 </script>
     
 <template>
   <div class="container d-flex flex-grow-1 flex-column">
+    <div class="d-flex flex-row justify-content-between align-items-center">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="show_labels" v-model="show_labels" @change="on_change_show_labels">
+        <label class="form-check-label" for="show_labels">Show labels</label>
+      </div>
+    </div>
     <div class="flex-grow-1 position-relative">
       <div class="w-100 h-100 plot-div" ref="plot_div" :id="plot_div_id"></div>
       <div class="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column align-items-center justify-content-center loading" v-if="drawing_busy">
