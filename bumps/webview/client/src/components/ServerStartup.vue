79f195a5591536cc64a1e8870e5cc9033a974d44
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-05-31 10:59:34 -0400

    optionally show status updates on server loading

--- bumps/webview/client/src/components/ServerStartup.vue
+++ bumps/webview/client/src/components/ServerStartup.vue
@@ -7,6 +7,8 @@ const props = defineProps<{socket:AsyncSocket}>();
 
 const loading_dialog = ref<HTMLDivElement>();
 const connected = ref(false);
+const status_string = ref<string>();
+const percent = ref<number>();
 
 let modal: Modal;
 
@@ -26,6 +28,16 @@ props.socket.on('connect', () => {
   modal?.hide();
 });
 
+interface ServerStartupStatus {
+  status: string;
+  percent: number; // 0-100
+}
+
+props.socket.on('server_startup_status', (status: ServerStartupStatus) => {
+  status_string.value = status.status;
+  percent.value = status.percent;
+});
+
 console.log('connected handler registered.');
 
 </script>
@@ -38,10 +50,17 @@ console.log('connected handler registered.');
         <div class="modal-header">
           <h5 class="modal-title" id="serverStartupLabel">
             Server Connecting...
-            <div class="spinner-border text-primary" role="status">
+            <div class="spinner-border text-primary" role="status" v-if="percent === undefined">
               <span class="visually-hidden">Connecting...</span>
             </div>
           </h5>
+        </div>
+        <div class="modal-body" v-if="status_string !== undefined">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" :style="{ width: percent + '%' }" :aria-valuenow="percent"
+              aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <p>{{ status_string }}</p>
         </div>
       </div>
     </div>
