8a7665648e70a8f3df639eb479107370754c5886
bbm <brian.maranville@nist.gov>
2024-06-05 13:04:56 -0400

    add History panel to panels

--- bumps/webview/client/src/panels.mjs
+++ bumps/webview/client/src/panels.mjs
@@ -8,11 +8,13 @@ import CorrelationView from './components/CorrelationViewPlotly.vue';
 import ParameterTraceView from './components/ParameterTraceView.vue';
 import ModelUncertaintyView from './components/ModelUncertaintyView.vue';
 import UncertaintyView from './components/UncertaintyView.vue';
+import History from './components/History.vue';
 
 export const panels = [
     {title: 'Data', component: DataView},
     {title: 'Summary', component: SummaryView},
     {title: 'Log', component: LogView},
+    {title: 'History', component: History},
     {title: 'Convergence', component: ConvergenceView},
     {title: 'Model', component: ModelInspect},
     {title: 'Parameters', component: ParameterView},
