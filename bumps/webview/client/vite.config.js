cd16db829dfb001fb1b167f0264370b34cbdd24e
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-08-31 09:42:09 -0400

    remove standalone web-browser build (separated project)

--- bumps/webview/client/vite.config.js
+++ bumps/webview/client/vite.config.js
@@ -14,16 +14,12 @@ export default ({mode}) => {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
-        './asyncSocket.ts': (mode == 'standalone') ? join(process.cwd(), 'src', 'asyncWorkerSocket.ts') : './asyncSocket.ts',
-        'socket.io-client': (mode == 'standalone') ? join(process.cwd(), 'src', 'asyncWorkerSocket.ts') : 'socket.io-client',
       }
     },
     define: {
       // By default, Vite doesn't include shims for NodeJS/
       // necessary for segment analytics lib to work
       global: {},
-      APP_VERSION: JSON.stringify(process.env.npm_package_version),
-      BUMPS_WHEEL_FILE: JSON.stringify(process.env.BUMPS_WHEEL_FILE),
     },
     worker: {
       format: 'es',
@@ -35,7 +31,7 @@ export default ({mode}) => {
       rollupOptions: {
         output: {
           // Default
-          dir: (mode == 'standalone') ? 'dist_standalone': join('dist', process.env.npm_package_version),
+          dir: join('dist', process.env.npm_package_version),
           entryFileNames: (mode == 'production') ? 'assets/[name].js' : 'assets/[name].[hash].js',
           assetFileNames: (mode == 'production') ? 'assets/[name][extname]' : undefined,
           // chunkFileNames: "chunk-[name].js",
