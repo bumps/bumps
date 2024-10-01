027a02acaa22b281d3d346b43ea01220d144a291
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-10-01 17:09:05 -0400

    use standard color scheme

---
+++ bumps/webview/server/colors.py
@@ -0,0 +1,2 @@
+# Colorblind-friendly colors, as per https://gist.github.com/thriveth/8560036
+COLORS = ["#377eb8", "#ff7f00", "#4daf4a", "#f781bf", "#a65628", "#984ea3", "#999999", "#e41a1c", "#dede00"]
