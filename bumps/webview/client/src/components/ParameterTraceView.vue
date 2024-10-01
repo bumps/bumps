c328d8e27114694670e562d4513641b72ef53682
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-10-01 15:29:02 -0400

    update parameter trace plot to use plotly and select parameter of interest

--- bumps/webview/client/src/components/ParameterTraceView.vue
+++ bumps/webview/client/src/components/ParameterTraceView.vue
@@ -1,36 +1,64 @@
 <script setup lang="ts">
 /// <reference types="@types/uuid"/>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { Socket } from 'socket.io-client';
 import { v4 as uuidv4 } from 'uuid';
 import { setupDrawLoop } from '../setupDrawLoop';
-import * as mpld3 from 'mpld3';
+import * as Plotly from 'plotly.js/lib/core';
+import { configWithSVGDownloadButton } from '../plotly_extras.mjs';
 
 const plot_div = ref<HTMLDivElement>();
 const plot_div_id = ref(`div-${uuidv4()}`);
+const current_index = ref<number>(0);
+const parameters_local = ref<String[]>([]);
 const props = defineProps<{
   socket: Socket,
 }>();
 
-setupDrawLoop('updated_uncertainty', props.socket, fetch_and_draw);
+const { draw_requested } = setupDrawLoop('updated_uncertainty', props.socket, fetch_and_draw);
 
-type MplD3PlotData = {
-  width?: number,
-  height?: number,
+async function get_parameter_names() {
+  const new_pars = await props.socket.asyncEmit("get_parameter_labels") as String[];
+  if (new_pars == null) {
+    return;
+  }
+  if (current_index.value > parameters_local.value.length) {
+    current_index.value = 0
+  }
+  parameters_local.value = new_pars
 }
 
+props.socket.on('model_loaded', get_parameter_names);
+
+onMounted(async () => {
+  await get_parameter_names();
+});
+
 async function fetch_and_draw() {
-  const payload = await props.socket.asyncEmit('get_parameter_trace_plot') as MplD3PlotData;
+
+  const payload = await props.socket.asyncEmit('get_parameter_trace_plot', current_index.value) as Plotly.PlotlyDataLayoutConfig;
   let plotdata = { ...payload };
-  plotdata.width = Math.round(plot_div.value?.clientWidth ?? 640) - 16;
-  plotdata.height = Math.round(plot_div.value?.clientHeight ?? 480) - 16;
-  mpld3.draw_figure(plot_div_id.value, plotdata, false, true);
+  const { data, layout } = plotdata;
+  const config: Partial<Plotly.Config> = {
+    responsive: true,
+    ...configWithSVGDownloadButton
+  }
+
+  await Plotly.react(plot_div.value as HTMLDivElement, [...data], layout, config);
+
 }
 
 </script>
     
 <template>
   <div class="container d-flex flex-grow-1 flex-column">
+    <select
+      v-model="current_index"
+      @change="draw_requested = true"
+      >
+      <option v-for="(parameter, index) in parameters_local" :key="index" :value="index">
+        {{ parameter ?? "" }} </option>
+    </select>
     <div class="flex-grow-1" ref="plot_div" :id="plot_div_id">
     </div>
   </div>
