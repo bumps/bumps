ee7999d4bd69fc4c2b8354888d95bed8f909cea6
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-10-01 17:04:50 -0400

    add opacity slider for dense trace plots

--- bumps/webview/client/src/components/ParameterTraceView.vue
+++ bumps/webview/client/src/components/ParameterTraceView.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 /// <reference types="@types/uuid"/>
-import { ref, onMounted } from 'vue';
+import { ref, shallowRef, onMounted } from 'vue';
 import { Socket } from 'socket.io-client';
 import { v4 as uuidv4 } from 'uuid';
 import { setupDrawLoop } from '../setupDrawLoop';
 import * as Plotly from 'plotly.js/lib/core';
 import { configWithSVGDownloadButton } from '../plotly_extras.mjs';
 
+const plot_data = shallowRef<Plotly.Data[]>([]);
+const plot_layout = shallowRef<Partial<Plotly.Layout>>();
+const plot_config = shallowRef<Partial<Plotly.Config>>();
 const plot_div = ref<HTMLDivElement>();
 const plot_div_id = ref(`div-${uuidv4()}`);
 const current_index = ref<number>(0);
 const parameters_local = ref<String[]>([]);
+const trace_opacity = ref(0.4);
 const props = defineProps<{
   socket: Socket,
 }>();
@@ -37,14 +41,31 @@ onMounted(async () => {
 async function fetch_and_draw() {
 
   const payload = await props.socket.asyncEmit('get_parameter_trace_plot', current_index.value) as Plotly.PlotlyDataLayoutConfig;
+
   let plotdata = { ...payload };
   const { data, layout } = plotdata;
+  plot_data.value = data
+  plot_layout.value = layout
   const config: Partial<Plotly.Config> = {
     responsive: true,
     ...configWithSVGDownloadButton
   }
 
-  await Plotly.react(plot_div.value as HTMLDivElement, [...data], layout, config);
+  plot_config.value = config
+
+  plot_data.value = data
+
+  await draw_plot()
+
+}
+
+async function draw_plot() {
+  const opacity = trace_opacity.value
+
+  let data = plot_data.value
+  data.forEach(line => line.opacity = opacity)
+  
+  await Plotly.react(plot_div.value as HTMLDivElement, [...data], plot_layout.value, plot_config.value);
 
 }
 
@@ -59,6 +80,14 @@ async function fetch_and_draw() {
       <option v-for="(parameter, index) in parameters_local" :key="index" :value="index">
         {{ parameter ?? "" }} </option>
     </select>
+    <div class="row px-2 align-items-center">
+      <div class="col-auto">
+        <label for="opacity_control" class="col-form-label">Trace opacity</label>
+      </div>
+      <div class="col">
+        <input type="range" min="0" max="1.0" step="0.01" id="opacity_control" class="form-range" v-model.number="trace_opacity" @input="draw_plot">
+      </div>
+    </div>
     <div class="flex-grow-1" ref="plot_div" :id="plot_div_id">
     </div>
   </div>
