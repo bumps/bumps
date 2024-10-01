a2d49e266c1762ba1123b7bf9da2e543b3c4632c
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-10-01 17:09:05 -0400

    use standard color scheme

--- bumps/webview/server/traceplot.py
+++ bumps/webview/server/traceplot.py
@@ -5,18 +5,23 @@ Build trace plots (plotly)
 __all__ = ['plot_trace']
 
 import numpy as np
+from .colors import COLORS
 
 def plot_trace(x: list | np.ndarray, ys: list | np.ndarray, label='', alpha=0.4):
 
     import plotly.graph_objs as go
 
     traces = []
+    color_idx = 0
     for y in ys:
+        color = COLORS[color_idx % len(COLORS)]
         traces.append(go.Scatter(x=x,
                                 y=y,
                                 mode='lines',
                                 showlegend=False,
+                                line=dict(color=color),
                                 opacity=alpha))
+        color_idx += 1
 
     fig = go.Figure(data=traces)
 
