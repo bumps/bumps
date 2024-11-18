73b1ebcaa4112e4c48335e12858192c05e977618
bbm <brian.maranville@nist.gov>
2024-11-18 18:45:39 -0500

    add dry-run option to stepper compiler

--- bumps/dream/build_compiled.py
+++ bumps/dream/build_compiled.py
@@ -6,7 +6,7 @@ import sysconfig
 
 CC = sysconfig.get_config_vars().get('CC', os.environ.get('CC', 'gcc'))
 
-def compile_dll(nthreads=None, use_openmp=True):
+def compile_dll(nthreads=None, use_openmp=True, dry_run=False):
     num_cores = multiprocessing.cpu_count()
     if nthreads == 0:
         nthreads = num_cores
@@ -15,6 +15,10 @@ def compile_dll(nthreads=None, use_openmp=True):
     openmp_flag = "-fopenmp" if use_openmp else ""
     flags = f"-I ../../Random123/include/ -O2 {openmp_flag} -shared -lm -o _compiled.so -fPIC -DMAX_THREADS={nthreads}"
     compile_command = f"{CC} compiled.c {flags}"
+    if dry_run:
+        print(f"Would compile: {compile_command}")
+        return False
+
     print(f"Compiling: {compile_command}")
     os.system(compile_command)
 
@@ -30,14 +34,15 @@ def remove_dll():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--nthreads', type=int, default=64, help='Number of threads to compile for, use 0 for all available cores')
+    parser.add_argument('--nthreads', type=int, default=64, help='Number of threads to compile for, use 0 for all available cores, default is 64')
     parser.add_argument('--no-openmp', action='store_true', help='Disable OpenMP (e.g. for clang, where it is not supported)')
     parser.add_argument('--remove', action='store_true', help='Remove the compiled dll')
+    parser.add_argument('--dry-run', action='store_true', help='Print the compile command without running it')
     args = parser.parse_args()
     if args.remove:
         remove_dll()
     else:
-        compile_dll(nthreads=args.nthreads, use_openmp=not args.no_openmp)
+        compile_dll(nthreads=args.nthreads, use_openmp=not args.no_openmp, dry_run=args.dry_run)
 
 if __name__ == "__main__":
     main()
