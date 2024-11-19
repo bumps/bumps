a33ec42303ed5597424628e6a9d416676f88fb41
bbm <brian.maranville@nist.gov>
2024-11-19 16:00:05 -0500

    fix type of passthrough ref in menu_items

--- bumps/webview/client/src/app_state.ts
+++ bumps/webview/client/src/app_state.ts
@@ -1,6 +1,6 @@
 import { ref, shallowRef } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
-import type { Ref } from 'vue';
+import type { ComputedRef, Ref } from 'vue';
 import type { AsyncSocket } from './asyncSocket.ts';
 
 interface ModalDialog {
@@ -36,7 +36,7 @@ export const default_fitter_settings = shallowRef<{ [fit_name: string]: FitSetti
 export const selected_fitter = ref<string>();
 export const default_fitter = "amoeba";
 export const notifications = ref<{ title: string, content: string, id: string, spinner: boolean }[]>([]);
-export const menu_items = ref<{disabled?: Ref<boolean> | boolean, text: string, action: Function, help?: string}[]>([]);
+export const menu_items = ref<{disabled?: Ref<ComputedRef<boolean>> | boolean, text: string, action: Function, help?: string}[]>([]);
 export const autosave_history = ref(false);
 export const autosave_history_length = ref(10);
 export const session_output_file = shallowRef<{ filename: string, pathlist: string[] }>();
