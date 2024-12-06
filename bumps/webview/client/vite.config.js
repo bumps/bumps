7e3dc53724c6c240fff5e716c49dd5d9ffd2f083
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-12-06 16:55:09 -0500

    simplify, not building in custom versioned folder

--- bumps/webview/client/vite.config.js
+++ bumps/webview/client/vite.config.js
@@ -1,41 +1,9 @@
-import { join } from "node:path";
-import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [vue(), svgLoader()],
-    base: "",
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    define: {
-      // By default, Vite doesn't include shims for NodeJS/
-      // necessary for segment analytics lib to work
-      global: {},
-    },
-    worker: {
-      format: "es",
-      rollupOptions: {
-        external: ["node-fetch"],
-      },
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          // Default
-          dir: join("dist", process.env.npm_package_version),
-          entryFileNames: mode == "production" ? "assets/[name].js" : "assets/[name].[hash].js",
-          assetFileNames: mode == "production" ? "assets/[name][extname]" : undefined,
-          // chunkFileNames: "chunk-[name].js",
-          // manualChunks: undefined,
-        },
-      },
-    },
-  });
-};
+export default defineConfig({
+  plugins: [vue(), svgLoader()],
+  base: "",
+});
