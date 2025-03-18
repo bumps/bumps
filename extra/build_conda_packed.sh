02663078ce575a0aa6e7db32818eb23128d63839
Brian Benjamin Maranville <brian.maranville@nist.gov>
2025-03-18 16:01:07 -0400

    simplified build_conda_packed.sh

--- extra/build_conda_packed.sh
+++ extra/build_conda_packed.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
 # Definitions
-ENV_NAME="isolated-base"
 PYTHON_VERSION="3.12"
 OUTPUT="conda_packed"
 SCRIPT_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SRC_DIR=$(dirname "$SCRIPT_DIR")
-pkgdir="$SRC_DIR/$OUTPUT"
 
 eval "$(conda shell.bash hook)"
 
-if ! test -f "$ENV_NAME.tar.gz"; then
-  conda install -y conda-pack
-  echo "creating isolated environment"
-  conda remove -n "$ENV_NAME" -y --all
-  conda create -n "$ENV_NAME" -q --force -y "python=$PYTHON_VERSION" "nodejs" "micromamba"
-  conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
+if ! command -v conda-pack &> /dev/null; then
+  echo "conda-pack is not installed. Please install it with 'conda install -y conda-pack'"
+  exit 1
 fi
 
-# unpack the new environment, that contains only python + pip
-# first, clean out the packed folder:
-rm -rf "$pkgdir"
-mkdir -p "$pkgdir"
-tar -xzf "$ENV_NAME.tar.gz" -C "$pkgdir"
+ISOLATED_ENV="$(mktemp -d)/env"
+conda create -y -p "$ISOLATED_ENV" "python=$PYTHON_VERSION" "nodejs" "micromamba" "pip"
 
-# add any icons
-mkdir -p $pkgdir/share/icons
-cp $SCRIPT_DIR/*.svg $pkgdir/share/icons
-cp $SCRIPT_DIR/*.png $pkgdir/share/icons
-cp $SCRIPT_DIR/*.ico $pkgdir/share/icons
-cp $SCRIPT_DIR/*.icns $pkgdir/share/icons
+cd $SCRIPT_DIR
+echo "Installing package in isolated environment, $(pwd)"
+conda activate "$ISOLATED_ENV"
 
-cd $SRC_DIR
-conda activate $pkgdir
-python -m pip install --no-input --no-compile .[webview]
+python -m pip install --no-input --no-compile "..[webview]"
+python -m "${PACKAGE_NAME:-bumps}.webview.build_client" --cleanup --install-dependencies
 
-# build the client
-cd $pkgdir
-python -m bumps.webview.build_client --cleanup
+conda deactivate
+conda-pack -p "$ISOLATED_ENV" --format=no-archive --output="$SRC_DIR/$OUTPUT"
 
+rm -rf "$ISOLATED_ENV"
 # done
