58db5afa367ab9ba27e185248e6d276c20ec696c
Paul Kienzle <pkienzle@nist.gov>
2025-04-02 16:17:17 -0400

    Fix errors introduced by code cleanup

--- bumps/webview/server/names.py
+++ bumps/webview/server/names.py
@@ -1,2 +1,3 @@
+# TODO: is this being used anywhere?
 from .api import state, set_problem, load_session
-from .webserver import main, start_app, create_server_task, sio
+from .webserver import start_app, create_server_task
