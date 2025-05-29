37c3141187a9288033d846914dfd493cd3152f7b
bbm <brian.maranville@nist.gov>
2025-05-29 17:56:15 -0400

    separate calc_errors and show_errors and shorten names

--- bumps/plugin.py
+++ bumps/plugin.py
@@ -45,7 +45,7 @@ import importlib.metadata
 import logging
 import traceback
 
-from typing import Callable, Dict, Generic, List, Optional, Protocol, TypeVar, TYPE_CHECKING
+from typing import Any, Callable, Dict, Generic, List, Optional, Protocol, TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from bumps.fitproblem import FitProblem
@@ -83,7 +83,7 @@ function that takes a bumps-serialized (JSON) dictionary and applies
 transformations to migrate older versions to the current schema required
 for the library, returning the transformed dictionary.
 """
-MIGRATION_PLUGINS: Dict[str, Callable[[dict], dict]] = load_plugin_group("bumps.serialization.migration")
+MIGRATION: Dict[str, Callable[[dict], dict]] = load_plugin_group("bumps.serialization.migration")
 
 
 """
@@ -91,38 +91,46 @@ Every registered bumps.model.load plugin should be a function that takes
 a filename and returns a model object or None if the file does not
 contain a model of the appropriate type.
 """
-MODEL_LOADER_PLUGINS: Dict[str, Callable[[str], "FitProblem"]] = load_plugin_group("bumps.model.load")
+MODEL_LOADER: Dict[str, Callable[[str], "FitProblem"]] = load_plugin_group("bumps.model.load")
 
 
 """
 Every registered bumps.model.new plugin should be a function that returns
 a new model object.
 """
-NEW_MODEL_PLUGINS: Dict[str, Callable[[], "FitProblem"]] = load_plugin_group("bumps.model.new")
+NEW_MODEL: Dict[str, Callable[[], "FitProblem"]] = load_plugin_group("bumps.model.new")
 
 
 """
-Every registered bumps.errplot.matplotlib plugin should be a function that
-takes a FitProblem, sample data, and an optional matplotlib Axes,
-and displays the model with uncertainty on that Axes object (or the current axes if None).
+Every registered bumps.calc_errors plugin should be a function
+that takes a FitProblem and a set of points in parameter space,
+and returns an object representing errors for the model at those points.
+(that object will be plotted by the show_errors plugin).
 """
-ERRPLOT_PLUGINS: Dict[str, Callable[["FitProblem", "np.ndarray", Optional["plt.Axes"]], None]] = load_plugin_group(
-    "bumps.errplot.matplotlib"
-)
+CALC_ERRORS: Dict[str, Callable[["FitProblem", "np.ndarray"], Any]] = load_plugin_group("bumps.calc_errors")
+
+
+"""
+Every registered bumps.show_errors.matplotlib plugin should be a function
+that takes an object representing errors and an optional matplotlib Axes,
+and displays the confidence regions on that Axes object (or the current axes if None).
+(The object is the output of calc_errors).
+"""
+SHOW_ERRORS: Dict[str, Callable[[Any, Optional["plt.Axes"]], None]] = load_plugin_group("bumps.show_errors.matplotlib")
 
 
 """
 Every registered bumps.wx_gui.data_view plugin should be a wx.Panel
 that provides a data view for the FitProblem.
 """
-DATA_VIEW_PLUGINS: Dict[str, "wx.Panel"] = load_plugin_group("bumps.wx_gui.data_view")
+DATA_VIEW: Dict[str, "wx.Panel"] = load_plugin_group("bumps.wx_gui.data_view")
 
 
 """
 Every registered bumps.wx_gui.model_view plugin should be a wx.Panel
 that provides a model view for the FitProblem.
 """
-MODEL_VIEW_PLUGINS: Dict[str, "wx.Panel"] = load_plugin_group("bumps.wx_gui.model_view")
+MODEL_VIEW: Dict[str, "wx.Panel"] = load_plugin_group("bumps.wx_gui.model_view")
 
 
 """
@@ -130,7 +138,7 @@ Every registered bumps.serialize.save_json plugin should be a function
 that takes a FitProblem and a path string, and saves the FitProblem
 as a JSON file to that path.
 """
-SAVE_JSON_PLUGINS: Dict[str, Callable[["FitProblem", str], None]] = load_plugin_group("bumps.serialize.save_json")
+SAVE_JSON: Dict[str, Callable[["FitProblem", str], None]] = load_plugin_group("bumps.serialize.save_json")
 
 
 # TODO: refl1d wants to do the following after cli.getopts()
