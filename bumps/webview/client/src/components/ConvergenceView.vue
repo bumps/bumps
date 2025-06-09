528c7dee455af1fb0ca7e5f3f58a29fd0c5d2e0d
bbm <brian.maranville@nist.gov>
2025-06-09 12:25:25 -0400

    add slider to control cutoff fraction at the beginning of plot

--- bumps/webview/client/src/components/ConvergenceView.vue
+++ bumps/webview/client/src/components/ConvergenceView.vue
@@ -7,14 +7,15 @@ import { setupDrawLoop } from "../setupDrawLoop";
 
 const title = "Convergence";
 const plot_div = ref<HTMLDivElement>();
+const cutoff = ref(0.25);
 const props = defineProps<{
   socket: AsyncSocket;
 }>();
 
-setupDrawLoop("updated_convergence", props.socket, fetch_and_draw, title);
+const { draw_requested } = setupDrawLoop("updated_convergence", props.socket, fetch_and_draw, title);
 
 async function fetch_and_draw() {
-  const payload = (await props.socket.asyncEmit("get_convergence_plot")) as Plotly.PlotlyDataLayoutConfig;
+  const payload = (await props.socket.asyncEmit("get_convergence_plot", cutoff.value)) as Plotly.PlotlyDataLayoutConfig;
   let plotdata = { ...payload };
   const { data, layout } = plotdata;
   const config = { responsive: true, scrollZoom: true, modeBarButtonsToAdd: [SVGDownloadButton] };
@@ -27,10 +28,32 @@ async function fetch_and_draw() {
     await Plotly.react(plot_div.value, [...data], layout, config);
   }
 }
+
+function draw_plot() {
+  draw_requested.value = true;
+}
 </script>
 
 <template>
   <div class="container d-flex flex-grow-1 flex-column">
+    <div class="row px-2 align-items-center">
+      <div class="col-auto">
+        <label for="cutoff_control" class="col-form-label">Cutoff: </label>
+        <span class="pl-1">{{ cutoff.toFixed(2) }}</span>
+      </div>
+      <div class="col">
+        <input
+          id="cutoff_control"
+          v-model.number="cutoff"
+          type="range"
+          min="0"
+          max="1.0"
+          step="0.01"
+          class="form-range"
+          @input="draw_plot"
+        />
+      </div>
+    </div>
     <div ref="plot_div" class="flex-grow-1"></div>
   </div>
 </template>
