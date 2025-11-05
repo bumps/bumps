e19706f82d8cffd5722808ee8ea3541cec3659cc
Brian Benjamin Maranville <brian.maranville@nist.gov>
2025-11-04 22:13:27 -0500

    set document.title to be the filename of the model, on first page load and then when filename changes

--- bumps/webview/client/src/App.vue
+++ bumps/webview/client/src/App.vue
@@ -36,7 +36,7 @@ const props = defineProps<{
   panels: Panel[];
   socket: AsyncSocket;
   singlePanel?: string;
-  name?: string;
+  name: string;
 }>();
 
 const show_menu = ref(false);
@@ -75,6 +75,9 @@ socket.on("connect", async () => {
   socket.asyncEmit("get_fitter_defaults", (new_fitter_defaults: { [fit_name: string]: FitSetting }) => {
     default_fitter_settings.value = new_fitter_defaults;
   });
+  socket.asyncEmit("get_shared_setting", "model_file", ({ filename }) => {
+    document.title = filename ? `${props.name} - ${filename}` : props.name;
+  });
 });
 
 socket.on("disconnect", (payload) => {
@@ -87,6 +90,10 @@ socket.on("disconnect", (payload) => {
 
 socket.on("add_notification", addNotification);
 socket.on("cancel_notification", cancelNotification);
+
+socket.on("model_file", ({ filename }) => {
+  document.title = filename ? `${props.name} - ${filename}` : props.name;
+});
 
 // function disconnect() {
 //   socket.disconnect();
